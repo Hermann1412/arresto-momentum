fcda3edf3e0656564af3f82c9a3a02fe0078f1a0
Hermann <u6411076@au.edu>
2026-05-23 00:11:09 +0700

    icons

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      <h1 className="text-3xl font-bold">arresto momentum</h1>
-      <Button variant="destructive" className="px-10">Delete</Button>
+      
     </main>
   )
 }
